3a74fc6cbe80c5270dab78b879e3921f846a59f5
Rahul Gupta <rgcool1999@gmail.com>
2025-10-12 23:42:16 +0530

    Remove unnecessary meta tags from index.html and delete robots.txt; simplify index.js by removing React.StrictMode

--- basicreactapp/src/index.js
+++ basicreactapp/src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import Home from './Home';  
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <>
       <App />
       <Home/>
     </>
-    
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
